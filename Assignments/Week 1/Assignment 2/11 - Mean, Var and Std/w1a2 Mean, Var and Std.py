b89be1be9183617d53385b8bd1dde02b21cea5c8
TheRandomizer7 <yash.yelmame@gmail.com>
2021-07-15 16:05:45 +0530

    done

---
+++ Assignments/Week 1/Assignment 2/11 - Mean, Var and Std/w1a2 Mean, Var and Std.py
@@ -0,0 +1,21 @@
+import numpy
+
+dimensions = input().strip().split(' ')
+rows = int(dimensions[0])
+columns = int(dimensions[1])
+
+arr = numpy.empty(rows * columns, numpy.int64)
+arr = arr.reshape(rows, columns)
+
+i = 0
+while i < rows:
+    input_column = input().strip().split()
+    j = 0
+    while j < columns:
+        arr[i, j] = int(input_column[j])
+        j += 1
+    i += 1
+
+print(numpy.mean(arr, axis = 1))
+print(numpy.var(arr, axis = 0))
+print(round(numpy.std(arr), 11))
