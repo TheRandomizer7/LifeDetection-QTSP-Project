5e2c1491aac018eec542cb02bd82b2bf87d2f50c
TheRandomizer7 <yash.yelmame@gmail.com>
2021-07-15 15:40:25 +0530

    bruh

---
+++ Assignments/Week 1/Assignment 2/7 - Array Mathematics/w1a2 Array Mathematics.py
@@ -0,0 +1,37 @@
+import numpy
+
+dimensions = input().strip().split(' ')
+rows = int(dimensions[0])
+columns = int(dimensions[1])
+
+
+arr1 = numpy.empty(rows * columns, numpy.int64)
+arr1 = arr1.reshape(rows, columns)
+
+arr2 = numpy.empty(rows * columns, numpy.int64)
+arr2 = arr2.reshape(rows, columns)
+
+i = 0
+while i < rows:
+    input_column = input().strip().split()
+    j = 0
+    while j < columns:
+        arr1[i, j] = int(input_column[j])
+        j += 1
+    i += 1
+
+i = 0
+while i < rows:
+    input_column = input().strip().split()
+    j = 0
+    while j < columns:
+        arr2[i, j] = int(input_column[j])
+        j += 1
+    i += 1
+
+print(arr1 + arr2)
+print(arr1 - arr2)
+print(arr1 * arr2)
+print(arr1 // arr2)
+print(arr1 % arr2)
+print(arr1 ** arr2)
